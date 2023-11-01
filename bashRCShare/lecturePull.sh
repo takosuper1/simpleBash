46e623973126822f72c2ebf747d6ffc43cf9f259
Gregg Sanders <arspoesis@gmail.com>
2023-10-31 21:45:37 -0400

    updated success logging in scripts

--- bashRCShare/lecturePull.sh
+++ bashRCShare/lecturePull.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-cd /c/Users/Student/source/repos/individual/your-name-student-code &&
+cd /c/Users/Student/source/repos/individual/gregg-sanders-student-code &&
 git add -A;
 git commit -m "prepull";
 git pull upstream main;
-#logging is bad. need to fix
-date -u |echo >> myscriptslog.txt;
-#logging is bad. need to fix
-echo "lecturePull: lecture pulled successfully" >> myscriptslog.txt
+echo "lecturePull: lecture pulled successfully $(date -u)" >> myscriptslog.txt
