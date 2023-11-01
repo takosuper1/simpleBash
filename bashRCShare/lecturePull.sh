00ec1b92c37db6064a38755aecc71ecec57aad75
TeacherOfLegend <37944471+takosuper1@users.noreply.github.com>
2023-10-31 21:29:49 -0400

    Update lecturePull.sh

--- bashRCShare/lecturePull.sh
+++ bashRCShare/lecturePull.sh
@@ -4,5 +4,7 @@ cd /c/Users/Student/source/repos/individual/your-name-student-code &&
 git add -A;
 git commit -m "prepull";
 git pull upstream main;
+#logging is bad. need to fix
 date -u |echo >> myscriptslog.txt;
+#logging is bad. need to fix
 echo "lecturePull: lecture pulled successfully" >> myscriptslog.txt
